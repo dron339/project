b25f083f634f85209d9f0febeb626d2e497622c6
Andrew <steamakaunt228@gmail.com>
2021-05-21 12:50:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,101 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log(4 / 0);
+// console.log('string' * 9);
+
+// const person = "Alex";
+
+// const bool = true;
+
+// //console.log(somthing);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+// const result = +prompt("Вам есть 18 ?", "18");
+// console.log(typeof(result));
+// console.log(result + 5);
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+// document.write(answers);
+
+
+// const movie = {
+//     name: 'Avengers',
+//     getName: function() {
+//         return this.name
+//     }
+// }
+// const movieName = movie.getName;
+// console.log(movieName());
+// console.log(movie.getName());
+
+// [1, 2, 3, 4] .reduce ((a, b) => console.log (a, b));
+
+// [1, 2, 3, 4].map((a) => a * 2);
+
+// const coordinates = {
+//     x: function(){
+//         console.log(3);
+//         return this;
+//     },
+//     y: function(){
+//         console.log(4);
+//         return this;
+//     },
+//     z: function(){
+//         console.log(5);
+//         return this;
+//     },
+// };
+// coordinates.x().y().z();
+
+
+// const rara = [];
+// rara[0] = prompt('Name', '');
+// document.write(rara);
+// alert(`Твой ${rara}`);
+
+
+
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(11.5 % 2);
+
+console.log(2 + 2 * 2 != "6");
+
+// && i
+// || ili 
+
+const isCheked = true,
+    isClose = false;
+
+console.log(isCheked || !isClose);
